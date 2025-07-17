04f69856db42b4393fd282b1ee25dd610b4fd701
Maura <maurashbkt@gmail.com>
2025-07-17 12:24:09 +0700

    revisi scriptjs

--- public/script.js
+++ public/script.js
@@ -1,9 +1,10 @@
-fetch('/api/ideas?page[number]=1&page[size]=10&append[]=small_image&append[]=medium_image&sort=-published_at')
+fetch('http://localhost:3000/api/ideas?page[number]=1&page[size]=10&append[]=small_image&append[]=medium_image&sort=-published_at')
   .then(res => res.json())
   .then(data => {
     console.log('Data dari API proxy:', data);
-   
-  });
+  
+  })
+  .catch(err => console.error('Error:', err));
 
 
 
